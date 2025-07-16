a67268b49ff046b6f43211b9c388c9961ccfdbdf
timo <valentin.timofte00@gmail.com>
2025-07-16 16:42:47 +0200

    add .env

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 import os
 
+
+from dotenv import load_dotenv
 from pyspark.sql import SparkSession
 
-
-os.environ["PYTHON_SPARK"] = ":C/Users/valen/Desktop/Projects/PySpark/venv/Scripts/python.exe"
-
-
-
-
-
+# Carica le variabili d'ambiente dal file .env
+# Questo renderà disponibili le variabili definite nel file .env come se fossero state impostate nel tuo terminale.
+load_dotenv()
 
 if __name__ == "__main__":
+    # 1. Leggiamo il percorso di Python dal nostro file .env
+    # Usiamo os.getenv() che restituisce None se la variabile non è trovata, evitando errori.
+    pyspark_python_path = os.getenv("PYTHON_SPARK")
+
     spark = SparkSession.builder \
                 .appName("MySpark") \
-                .getOrCreate()
+                .config("spark.pyspark.python", pyspark_python_path) \
+                .getOrCreate() 
 
     data = [(1, "Alice"), (2, "Bob"), (3, "Charlie")]
     df = spark.createDataFrame(data, ["id", "name"])
@@ -22,5 +25,3 @@ if __name__ == "__main__":
 
     spark.stop()
     
-
-
